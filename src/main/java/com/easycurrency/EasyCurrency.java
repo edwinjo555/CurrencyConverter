8d22bf1e18f457278797b1c5eb2b9c59dd27e29e
Sivakumar00 <siva.github00@gmail.com>
2018-06-29 09:33:42 +0530

    1.2

--- src/main/java/com/easycurrency/EasyCurrency.java
+++ src/main/java/com/easycurrency/EasyCurrency.java
@@ -15,6 +15,10 @@ import java.io.IOException;
 
 public class EasyCurrency {
 
+    public static void main(String []args){
+
+        System.out.println(getCurrencyExchange("eur","inr",5));
+    }
 
 
     public static double convertCurrency(String from, String to) {
@@ -42,6 +46,33 @@ public class EasyCurrency {
         JSONObject jsonObject = new JSONObject(responseString);
         Double output = jsonObject.getDouble(reqString);
         return output;
+
+    }
+    public static double getCurrencyExchange(String from, String to,int n) {
+
+        String API_ENDPOINT="https://free.currencyconverterapi.com/api/v5/convert?q=";
+        String reqString = (from + "_" + to).toUpperCase();
+
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(API_ENDPOINT + reqString + "&compact=ultra");
+        httpGet.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpResponse response = null;
+        try {
+            response = client.execute(httpGet);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+
+        }
+        HttpEntity entity = response.getEntity();
+        String responseString = null;
+        try {
+            responseString = EntityUtils.toString(entity, "UTF-8");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        JSONObject jsonObject = new JSONObject(responseString);
+        Double output = jsonObject.getDouble(reqString);
+        return output*n;
 
     }
 }
